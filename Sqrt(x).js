62045cc24b1f7830a64150a83e3f546166c3f521
StarCrew <starcrew@163.com>
2016-07-11 17:00:19 +0800

    Create Sqrt(x).js

---
+++ Sqrt(x).js
@@ -0,0 +1,12 @@
+/**
+ * Implement int sqrt(int x).
+ * Compute and return the square root of x.
+ */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x));
+};
